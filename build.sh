28a65a8677c0cfc4e04736591d1344790b6aba89
s1dpfy <eldotemp01@gmail.com>
2026-06-17 20:20:10 +0900

    edit admin command

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ echo "데이터베이스 마이그레이션 중..."
 python manage.py migrate
 
 echo "관리자 계정 자동 생성 중..."
-python manage.py createsuperuser --noinput || true
+python manage.py shell -c "import os; from django.contrib.auth import get_user_model; User = get_user_model(); username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin'); password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '12345678'); u, created = User.objects.get_or_create(username=username); u.set_password(password); u.is_superuser = True; u.is_staff = True; u.save(); print('세팅되었습니다!')"
